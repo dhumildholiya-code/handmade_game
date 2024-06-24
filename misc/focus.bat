d74d7798bbd437b2a243d932ece48e822354fb39
dhumildholiya97 <dhumildholiya97@gmail.com>
2024-06-24 10:02:37 +0530

    Setup editor build command

---
+++ misc/focus.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start "/k" "D:\Editor\focus.exe"
--- misc/shell.bat
+++ misc/shell.bat
@@ -1,3 +1,4 @@
 @echo off
 
+set path=D:\CppProjects\pong_game\misc;%path%
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
